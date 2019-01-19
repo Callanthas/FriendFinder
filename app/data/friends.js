661a54362c692adc5683510659168cc93ad82347
Callanthas <lord_of_light1@hotmail.com>
2019-01-19 01:05:10 -0600

    added home and survey html, server js and survey js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+      name: "Omar",
+      photo: "https://avatars3.githubusercontent.com/u/42875827?s=460&v=4",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+  ];
+  
+  module.exports = friends;
